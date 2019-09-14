782fc59aa0883ccce1d81a1b73c802af5126a722
Jansen <jansen.wu528@gmail.com>
2019-09-15 04:44:47 +0700

    change model baseline

--- baseline_model_train.py
+++ baseline_model_train.py
@@ -1,9 +1,10 @@
-# vgg16 model used for transfer learning on the dogs and cats dataset
+# baseline model with data augmentation for the dogs vs cats dataset
 import sys
 from matplotlib import pyplot
 from keras.utils import to_categorical
-from keras.applications.vgg16 import VGG16
-from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.optimizers import SGD
@@ -11,17 +12,16 @@ from keras.preprocessing.image import ImageDataGenerator
  
 # define cnn model
 def define_model():
-	# load model
-	model = VGG16(include_top=False, input_shape=(224, 224, 3))
-	# mark loaded layers as not trainable
-	for layer in model.layers:
-		layer.trainable = False
-	# add new classifier layers
-	flat1 = Flatten()(model.layers[-1].output)
-	class1 = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat1)
-	output = Dense(1, activation='sigmoid')(class1)
-	# define new model
-	model = Model(inputs=model.inputs, outputs=output)
+	model = Sequential()
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(1, activation='sigmoid'))
 	# compile model
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
@@ -48,20 +48,24 @@ def summarize_diagnostics(history):
 def run_test_harness():
 	# define model
 	model = define_model()
-	# create data generator
-	datagen = ImageDataGenerator(featurewise_center=True)
-	# specify imagenet mean values for centering
-	datagen.mean = [123.68, 116.779, 103.939]
-	# prepare iterator
-	train_it = datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
-		class_mode='binary', batch_size=64, target_size=(224, 224))
-	test_it = datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
-		class_mode='binary', batch_size=64, target_size=(224, 224))
+	# create data generators
+	train_datagen = ImageDataGenerator(rescale=1.0/255.0,
+		width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True)
+	test_datagen = ImageDataGenerator(rescale=1.0/255.0)
+	# prepare iterators
+	train_it = train_datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
+		class_mode='binary', batch_size=64, target_size=(200, 200))
+	test_it = test_datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
+		class_mode='binary', batch_size=64, target_size=(200, 200))
 	# fit model
-	model.fit_generator(train_it, steps_per_epoch=len(train_it),
-		validation_data=test_it, validation_steps=len(test_it), epochs=10, verbose=1)
-    # save model
-    model.save('vgg16-model.h5')
+	history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+		validation_data=test_it, validation_steps=len(test_it), epochs=50, verbose=0)
+	# evaluate model
+	_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
+    model.save('base_model.h5')
  
 # entry point, run the test harness
 run_test_harness()
